76cb394a29a235433a8f1532c0db29b0ef2b508a
Jirayut Keawchuen <jirayut_deknoi@hotmail.co.th>
2015-08-25 18:34:43 +0700

    570610558 Finish Lab 2-1

--- Lab201/Program.cs
+++ Lab201/Program.cs
@@ -1,6 +1,6 @@
 using System;
 
-namespace Lab2
+namespace Lab201
 {
 	class MainClass
 	{
@@ -21,12 +21,13 @@ namespace Lab2
 				Console.WriteLine (s);
 
 			Console.WriteLine ("\n--- Playing with s4, s5, s6 objects ---");
-			stds [4].Name = "Mario";
-			stds [5].YearOfBirth = 1900;
-			stds [6].isActive = false;
+			stds [4]._Name = "Mario";           
+            stds [5]._YearofBirth = 1900;            
+            stds [6]._isActive = false;
 			for (int i = 4; i < stds.Length; i++)
 				Console.WriteLine (stds [i]);
 
+            Console.ReadKey();
 		}
 	}
 }
---
+++ Lab201/Student.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab201
+{
+    class Student
+    {
+        private string Name;
+        private string StudentID;
+        private int YearofBirth;
+        private bool isActive;
+  
+        public string _Name
+        {
+            get { return _Name; }
+            set { Name = value; }             
+        }
+        public bool _isActive
+        {
+            get {return _isActive; }
+            set { isActive = value; }
+        } 
+        public int _YearofBirth
+        {
+            get { return _YearofBirth; }       
+            set
+            {
+                if (value < 1950 || value > 2000)
+                {
+                    Console.WriteLine(StudentID + " : error try setting invalid year-of-birth value!");
+                }
+                else
+                {
+                    YearofBirth = value;
+                }             
+            }
+        }
+        //default constuctor
+        public Student()
+        {
+            this.isActive = false;
+            this.Name = "John Doe";
+            this.YearofBirth = 1995;
+            this.StudentID = "unknow";
+        }
+
+        public Student(string n,string sid)
+        {
+            this.Name = n;
+            this.StudentID = sid;
+            this.YearofBirth = 1995;
+            this.isActive = true;
+        }
+        public Student(string n,string sid,int y)
+        {
+            this.Name = n;
+            this.StudentID = sid;
+            this.setyear(y);
+            this.isActive = true;
+        }
+        public Student(string n, string sid, int y,bool active)
+        {
+            this.Name = n;
+            this.StudentID = sid;
+            this.setyear(y);
+            this.isActive = active;
+        }
+        public void setyear(int year)
+        {
+            if(year>=1950&&year<=2000)
+            {
+                this.YearofBirth = year;
+            }
+            else
+            {
+                Console.WriteLine(StudentID + " : error try setting invalid year-of-birth value!");
+            }
+        }
+        public int getAge()
+        {  
+                return 2015 - YearofBirth;                       
+        }
+        public override string ToString()
+        {
+            string active=" ";
+            if(isActive==false)
+            {
+                active = "NOT";
+            }
+            string output = ("[Student: " + Name + "(" + StudentID + ")" + ", age=" + getAge()+ ", is " +active+ " active student");
+            return output;
+        }
+    }
+}
