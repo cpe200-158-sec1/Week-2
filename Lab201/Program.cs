ca48aff40325b5900e03111133868710f246a131
kyuuuyki <chayanon_ardkham@cmu.ac.th>
2015-08-25 16:29:00 +0700

    0559 LAB 2-1

--- Lab201/Program.cs
+++ Lab201/Program.cs
@@ -22,11 +22,11 @@ namespace Lab2
 
 			Console.WriteLine ("\n--- Playing with s4, s5, s6 objects ---");
 			stds [4].Name = "Mario";
-			stds [5].YearOfBirth = 1900;
+			stds [5]._YearOfBirth = 1900;
 			stds [6].isActive = false;
 			for (int i = 4; i < stds.Length; i++)
 				Console.WriteLine (stds [i]);
-
+            Console.ReadKey();
 		}
 	}
 }
