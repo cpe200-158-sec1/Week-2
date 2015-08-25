00667b9efcb6a1d2b8e26fa8e8dc782f61eb0fdf
kaiyast <0kaiyast0@gmail.com>
2015-08-25 14:38:37 +0700

    570610540 lab1-2 fixxx

---
+++ Lab201/Student.cs
@@ -0,0 +1,70 @@
+﻿namespace Lab2
+{
+    class Student
+    {
+
+        public string Name, StudentID;
+        public int YearOfBirth; // YearOfBirth(from 1950 to 2000)
+        public bool isActive;
+
+        //1
+        public Student()
+        {
+            Name = "John Doe";
+            StudentID = "Unknow";
+            YearOfBirth = 1950;
+            isActive = false;
+        }
+        // Student(Name, StudentID) : takes 2 parameters
+        public Student(string Name, string StudentID)
+        {
+            this.Name = Name;
+            this.StudentID = StudentID;
+            YearOfBirth = 1950;
+            isActive = true;
+        }
+        // Student(Name, StudentID, YearOfBirth): takes 3 parameters
+        public Student(string Name, string StudentID, int YearOfBirth)
+        {
+            this.Name = Name;
+            this.StudentID = StudentID;
+            this.YearOfBirth = YearOfBirth;
+            isActive = true;
+        }
+        // Student(Name, StudentID, YearOfBirth, isActive): takes 4 parameters
+        public Student(string Name, string StudentID, int YearOfBirth, bool isActive)
+        {
+            this.Name = Name;
+            this.StudentID = StudentID;
+            this.YearOfBirth = YearOfBirth;
+            this.isActive = isActive;
+        }
+
+        //getAge(): calculate current age of student
+        public int getAge()
+        {
+            int age;
+
+            age = 2015 - YearOfBirth;
+
+            return age;
+        }
+
+        public override string ToString()
+        {
+            //[Student: s1 (57001), age=20, is active student]
+            string output = "";
+            output += "[Student: " + Name + " (" + StudentID + "), age=" + getAge() + ", is ";
+            if (!isActive)
+            {
+                output += "NOT ";
+            }
+            output += "active student]";
+
+            return output;
+        }
+        // ToString(): display object state in specify format(see expected output)
+
+
+    }
+}
