42d2877d48e411408f392fff197e890a6fb24ba6
thawanrat2609 <only_amp_me@hotmail.com>
2015-08-25 18:09:53 +0700

    lab2-1

---
+++ Lab201/Student.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab201
+{
+    class Student
+    {
+        private string student;
+        private string _name;
+        private int _studentID;
+        private int _age;
+        private int _yob;
+        
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
+        }
+
+        public int StudentID 
+        {
+            get
+            {
+                return _studentID;
+            }
+            set
+            {
+                _studentID = value;
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return _age;
+            }
+            set
+            {
+                if (1 < age || age > 100)
+                {
+
+                }
+                _age = value;
+            }
+        }
+
+        public string Student
+        {
+            get
+            {
+                _name = "John Doe";
+                _studentID = "(Unknown)";
+                _age = "20";          
+            }
+        }
+
+        public string YOB
+        {
+            get
+            {
+                return _yob;
+            }
+            set
+            {
+                if (2001 < yob || yob > 1949)
+                {
+
+                }
+                _age = value;
+
+
+
+
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+}
