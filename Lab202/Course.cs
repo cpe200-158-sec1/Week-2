b9e18afe7d84ce7877745c84760d1cf7693a6812
Watchanan Chanthaphakun <tmwatchanan@gmail.com>
2015-08-26 15:08:46 +0700

    Lab202 v.3 + Added comments to each part. * Improved the way to validate CourseID.

--- Lab202/Course.cs
+++ Lab202/Course.cs
@@ -8,12 +8,14 @@ namespace Lab202
 {
     class Course
     {
+        // Fields
         private string _name;
         private string _courseid;
         private string _lecturer;
         private int _maxstudents;
         private int _numstudents;
 
+        // Constructors
         public Course()
         {
             Name = "No Name";
@@ -47,11 +49,13 @@ namespace Lab202
             MaxStudents = pMaxStudents;
         }
 
+        // ToString method
         public override string ToString()
         {
             return string.Format("[Course: {0} ({1}), Lecturer={2}, has {3} students, max={4}]", Name, CourseID, Lecturer, NumStudents, MaxStudents);
         }
 
+        // Properties
         public string Name
         {
             get;
@@ -65,22 +69,15 @@ namespace Lab202
             }
             set
             {
-                if (value.Length != 6)
+                int coursenumber;
+                bool isNumeric = int.TryParse(value, out coursenumber);
+                if (value.Length != 6 || !isNumeric)
                 {
                     Console.WriteLine(CourseID + ": error try setting invalid CourseID!");
                 }
                 else
                 {
-                    int thatnumber;
-                    bool isNumeric = int.TryParse(value, out thatnumber);
-                    if (isNumeric)
-                    {
-                        _courseid = value;
-                    }
-                    else
-                    {
-                        Console.WriteLine(CourseID + ": error try setting invalid CourseID!");
-                    }
+                    _courseid = value;
                 }
             }
         }
